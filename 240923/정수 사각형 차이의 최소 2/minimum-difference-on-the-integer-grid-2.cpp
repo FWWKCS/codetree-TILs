78ff79d48d61fd40955d7344a6b1bc18cf0adb38
퍼박 <129183205+FWWKCS@users.noreply.github.com>
2024-09-23 00:51:21 +0900

    Title: 정수 사각형 차이의 최소 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240923/정수 사각형 차이의 최소 2/minimum-difference-on-the-integer-grid-2.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#define MAX 100
+using namespace std;
+
+int n;
+int table[MAX+1][MAX+1];
+int dp[MAX+1][MAX+1];
+
+void renew(int threshold) {
+    // 임계값 이상의 값을 가지는 영역만 건너면서 경로의 최댓값이 최소가 되도록
+    for (int i = 1; i <= n; i++) {
+        if (table[1][i] < threshold) dp[1][i] = -1;
+        else if (dp[1][i-1] == -1) dp[1][i] = -1;
+        else dp[1][i] = max(dp[1][i-1], table[1][i]);
+
+        if (table[i][1] < threshold) dp[i][1] = -1;
+        else if (dp[i-1][1] == -1) dp[i][1] = -1;
+        else dp[i][1] = max(dp[i-1][1], table[i][1]);
+    } 
+
+    for (int r = 2; r <= n; r++) {
+        for (int c = 2; c <= n; c++) {
+            if (table[r][c] < threshold) {
+                dp[r][c] = -1;
+                continue;
+            }
+
+            if (dp[r-1][c] == -1 && dp[r][c-1] == -1) {
+                dp[n][n] = MAX;
+                return;
+            }
+
+            if (dp[r-1][c] != -1 && dp[r][c-1] == -1) {
+                dp[r][c] = max(dp[r-1][c], table[r][c]);
+            }
+            else if (dp[r][c-1] != -1 && dp[r-1][c] == -1) {
+                dp[r][c] = max(dp[r][c-1], table[r][c]);
+            }
+            else {
+                dp[r][c] = max(table[r][c], min(dp[r-1][c], dp[r][c-1]));
+            }
+
+
+        }
+    }
+
+    
+}
+
+int main() {
+    cin >> n;
+
+    int minV = 100;
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            int x; cin >> x;
+            table[i][j] = x;
+            minV = min(minV, x);
+        }
+    }
+
+    int answer = MAX;
+    for (int i = minV; i <= table[1][1]; i++) {
+        // cout << i << '\n';
+        renew(i);
+        // for (int i = 1; i <= n; i++) {
+        // for (int j = 1; j <= n; j++) cout << dp[i][j] << ' ';
+        // cout << '\n';
+        // }
+
+        // cout << '\n';
+        answer = min(answer, dp[n][n] - i);
+    }
+
+    cout << answer;
+    return 0;
+}
