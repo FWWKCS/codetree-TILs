42fb6c31e7c261dfcbe976b43d0a5deb7c1f723e
KCS <129183205+FWWKCS@users.noreply.github.com>
2025-09-09 15:47:25 +0900

    Title: 계단 오르기, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250909/계단 오르기/climbing-stairs.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+
+int main() {
+    cin >> n;
+
+    // Please write your code here.
+    int dp[1001] = {0,};
+    dp[2] = dp[3] = 1;
+    for (int i = 4; i <= 1000; i++) {
+        dp[i] = dp[i-2] + dp[i-3];
+    }
+
+    cout << dp[n];
+    return 0;
+}
