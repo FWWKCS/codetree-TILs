a6fd352a8b3c0ae3d5ba320a1a6120b5b8d97383
KCS <129183205+FWWKCS@users.noreply.github.com>
2025-09-09 15:48:03 +0900

    Title: 계단 오르기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 250909/계단 오르기/climbing-stairs.cpp
+++ 250909/계단 오르기/climbing-stairs.cpp
@@ -11,7 +11,7 @@ int main() {
     int dp[1001] = {0,};
     dp[2] = dp[3] = 1;
     for (int i = 4; i <= 1000; i++) {
-        dp[i] = dp[i-2] + dp[i-3];
+        dp[i] = (dp[i-2] + dp[i-3]) % 10'007;
     }
 
     cout << dp[n];
