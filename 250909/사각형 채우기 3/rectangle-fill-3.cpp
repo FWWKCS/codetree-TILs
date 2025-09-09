35c984cc7ef4bdafb746d0b75505408f48917fc5
KCS <129183205+FWWKCS@users.noreply.github.com>
2025-09-09 18:10:21 +0900

    Title: 사각형 채우기 3, Time: 5ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 250909/사각형 채우기 3/rectangle-fill-3.cpp
+++ 250909/사각형 채우기 3/rectangle-fill-3.cpp
@@ -8,11 +8,13 @@ int main() {
     cin >> n;
 
     // Please write your code here.
-    int dp[1001] = {0, };
+    long long dp[1001] = {0, };
     dp[0] = 1;
     dp[1] = 2;
     for (int i = 2; i <= 1000; i++) {
         dp[i] = (((dp[i-1]*2) % MOD) + ((dp[i-2]*3) % MOD)) % MOD; 
+        if (i > 2) dp[i] += (dp[i-3] * 2) % MOD;
+        dp[i] %= MOD;
     }
     cout << dp[n];
     return 0;
