380ecca165dc854617e962108e0dbc6453245472
KCS <129183205+FWWKCS@users.noreply.github.com>
2025-09-24 14:06:40 +0900

    Title: 배낭 채우기, Time: 7ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250924/배낭 채우기/knapsack.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int N, M;
+int w[100], v[100];
+
+vector<int> dp;
+
+int main() {
+    cin >> N >> M;
+    dp.resize(M+1, -1);
+
+    for (int i = 0; i < N; i++) {
+        cin >> w[i] >> v[i];
+    }
+
+    dp[0] = 0;
+    // Please write your code here.
+    for (int i = 0; i < N; i++) {
+        for (int j = M; j > -1; j--) {
+            if (j-w[i] >= 0 && dp[j-w[i]] > -1) dp[j] = max(dp[j], dp[j-w[i]]+v[i]);
+        }
+    }
+
+    cout << dp[M];
+
+    return 0;
+}
